7efc4131b2e2f10b31f8afd59529b5846b8ea649
vyshalibalakrishnan <61009839+vyshalibalakrishnan@users.noreply.github.com>
2021-05-28 20:31:04 +0530

    Add files via upload

---
+++ helloworld/src/main/java/vys/helloworld/remo.java
@@ -0,0 +1,13 @@
+package vys.helloworld;
+
+public class remo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    int a=1;
+    int b=2;
+    int c=a+b;
+    System.out.println("The output of my game: "+c);
+	}
+
+}
